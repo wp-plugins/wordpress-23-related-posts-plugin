6c046353d681fbe8532bf8520ff1d2529b2494c3
denishua <denishua@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2007-09-16 10:51:54 +0000

    Add a option menu to customize all display text and if related posts shown in the feed
    git-svn-id: https://plugins.svn.wordpress.org/wordpress-23-related-posts-plugin/trunk@18940 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wp23_related_posts.php
+++ wp23_related_posts.php
@@ -1,12 +1,11 @@
 <?php
 /*
 Plugin Name: WP 2.3 Related Posts
-Version: 0.1
+Version: 0.2
 Plugin URI: http://fairyfish.net/2007/09/12/wordpress-23-related-posts-plugin/
 Description: Generate a related posts list via tags of WorPdress 2.3
-Author: Denis Deng
+Author: Denis,PaoPao
 Author URI: http://fairyfish.net/
-
 
 Copyright (c) 2007
 Released under the GPL license
@@ -29,18 +28,15 @@ http://www.gnu.org/licenses/gpl.txt
 
 	INSTALL: 
 	Just install the plugin in your blog and activate
-
 */
 
-function wp23_get_related_posts($limit = 5) {
+function wp23_get_related_posts() {
 	global $wpdb, $post;
-	
 	$now = current_time('mysql', 1);
-
 	$tags = wp_get_post_tags($post->ID);
-	
+
 	//print_r($tags);
-	
+
 	$taglist = "'" . str_replace("'",'',str_replace('"','',urldecode($tags[0]->term_id))). "'";
 	$tagcount = count($tags);
 	if ($tagcount > 1) {
@@ -49,10 +45,10 @@ function wp23_get_related_posts($limit = 5) {
 		}
 	}
 	
-
-	if ($limit != 0) {
-		$limitclause = "LIMIT $limit";
-	}
+	$limit = get_option("wp23_RP_limit");
+	
+	if ($limit = "")	$limit = 0;
+	if ($limit != 0) $limitclause = "LIMIT $limit";
 
 	$q = <<<SQL
 	SELECT DISTINCT p.*, count(t_r.object_id) as cnt
@@ -72,29 +68,108 @@ SQL;
 	return $wpdb->get_results($q);
 }
 
-function wp23_related_posts($limit = 5){
-	$relate_posts = wp23_get_related_posts($limit) ;
-	echo '<h3>Related Post</h3>';
-	echo '<ul class="related_post">';
-	foreach ($relate_posts as $relate_post ){
-		echo '<li><a href="'.get_permalink($relate_post->ID).'" title="'.$relate_post-> post_title .'">'.$relate_post-> post_title.'</a></li>';
-	}
-	echo '</ul>';
+function wp23_related_posts(){
+
+	$relate_posts = wp23_get_related_posts() ;
+	
+	if ($relate_posts){
+		$wp23_RP_title = get_option("wp23_RP_title");
+		if(!$wp23_RP_title) $wp23_RP_title= _e("Related Post");
+		
+		echo '<h3>'.$wp23_RP_title.'</h3>';
+		echo '<ul class="related_post">';
+		
+		foreach ($relate_posts as $relate_post ){
+			echo '<li><a href="'.get_permalink($relate_post->ID).'" title="'.$relate_post-> post_title .'">'.$relate_post-> post_title.'</a></li>';
+		}
+		
+		echo '</ul>';
+	} else {
+		$wp23_no_RP_text = get_option("wp23_no_RP_text");
+		if(!$wp23_no_RP_text) $wp23_RP_title= _e("No Related Post");
+		echo '<h3>'.$wp23_no_RP_text.'</h3>';
+	}	
 }
 
 function wp23_related_posts_for_feed($content=""){
-	if ( ! is_feed() ) return $content;
+	$wp23_RP_RSS = (get_option("wp23_RP_RSS") == 'yes') ? 1 : 0;
+	
+	if ( (! is_feed()) || (! $wp23_RP_RSS)) return $content;
 
 	$relate_posts = wp23_get_related_posts() ;
-	$content = $content . '<h3>Related Post</h3><ul class="related_post">';
-	foreach ($relate_posts as $relate_post ){
-		$content = $content . '<li><a href="'.get_permalink($relate_post->ID).'" title="'.$relate_post-> post_title .'">'.$relate_post-> post_title.'</a></li>';
+	if($relate_posts){
+		$wp23_RP_title = get_option("wp23_RP_title");
+		if(!$wp23_RP_title) $wp23_RP_title= _e("Related Post");
+		$content = $content . '<h3>'.$wp23_RP_title.'</h3><ul class="related_post">';
+		foreach ($relate_posts as $relate_post ){
+			$content = $content . '<li><a href="'.get_permalink($relate_post->ID).'" title="'.$relate_post->post_title .'">'.$relate_post-> post_title.'</a></li>';
+		}
+		$content = $content . '</ul>';
 	}
-	$content = $content . '</ul>';
 	
 	return $content;
 }
 
 add_filter('the_content', 'wp23_related_posts_for_feed',99);
 
-?>
+add_action('admin_menu', 'wp23_add_related_posts_options_page');
+function wp23_add_related_posts_options_page() {
+	if (function_exists('add_options_page')) {
+		add_options_page('WP23 Related Posts', 'WP23 Related Posts', 8, basename(__FILE__), 'wp23_related_posts_options_subpanel');
+	}
+}
+
+function wp23_related_posts_options_subpanel() {
+    if ($_POST['wp23_RP_stage'] == 'process') {
+        update_option('wp23_RP_RSS', $_POST['wp23_RP_RSS_option']);
+        update_option('wp23_RP_title', $_POST['wp23_RP_title_option']);
+		update_option('wp23_no_RP_text', $_POST['wp23_no_RP_text_option']);
+		update_option('wp23_RP_limit', $_POST['wp23_RP_limit_option']);
+	}
+?>
+    <div class="wrap">
+        <h2 id="write-post">Related Posts Options&hellip;</h2>
+        <p><?php _e("WordPress 2.3 Related Posts Plugin will generate a related posts via WordPress 2.3 tags, and add the related posts to feed.");?></p>
+        <form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>?page=<?php echo basename(__FILE__); ?>">
+            <input type="hidden" name="wp23_RP_stage" value="process" />
+            <fieldset class="options">
+                <legend><?php _e("Related Posts Preference");?></legend>
+                <table>
+                    <tr>
+                        <td valign="top" align="right"><?php _e("Related Posts Title:"); ?></td>
+                        <td>
+							<input type="text" name="wp23_RP_title_option" value="<?php echo get_option("wp23_RP_title"); ?>" />
+						</td>
+                    </tr>
+                    <tr>
+                        <td valign="top" align="right"><?php _e("No Related Posts Text:"); ?></td>
+                        <td>
+							<input type="text" name="wp23_no_RP_text_option" value="<?php echo get_option("wp23_no_RP_text"); ?>" />
+						</td>
+                    </tr>
+                    <tr>
+                        <td valign="top" align="right"><?php _e("Limit:");?></td>
+                        <td>
+							<input type="text" name="wp23_RP_limit_option" value="<?php echo get_option("wp23_RP_limit"); ?>" />
+						</td>
+                    </tr>
+					<tr>
+						<td valign="top" align="right">
+						<?php
+							if ( get_option("wp23_RP_RSS") == 'yes' ) {
+								echo '<input name="wp23_RP_RSS_option" type="checkbox" value="yes" checked>';
+							} else {
+								echo '<input name="wp23_RP_RSS_option" type="checkbox" value="yes">';
+							}
+						?>
+						</td>
+						<td>
+                        <?php _e("Related Posts for RSS");?>
+						</td>
+					</tr>
+                </table>
+            </fieldset>
+            <p class="submit"><input type="submit" value="Update Preferences &raquo;" name="Submit" /></p>
+        </form>
+    </div>
+<?php }?>
