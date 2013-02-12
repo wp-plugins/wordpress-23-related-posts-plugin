5996c29e6e8e92eff1421bd5d301870cd943b23d
jureham <jureham@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-02-12 22:44:56 +0000

    fixed defaults
    git-svn-id: https://plugins.svn.wordpress.org/wordpress-23-related-posts-plugin/tags/2.5@667102 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- config.php
+++ config.php
@@ -217,7 +217,7 @@ function wp_rp_install() {
 			'display_thumbnail'			=> false,
 			'display_excerpt'			=> false,
 			'excerpt_max_length'			=> 200,
-			'theme_name' 				=> 'vertical-m.css',
+			'theme_name' 				=> 'pinterest.css',
 			'theme_custom_css'			=> WP_RP_DEFAULT_CUSTOM_CSS,
 			'custom_theme_enabled' => false,
 		)
